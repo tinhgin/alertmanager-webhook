b14a43c6bc6be64325ef253b5ba7011bbea168ef
tinhhn3 <tinhhn3@fpt.com.vn>
2020-04-18 20:30:16 +0700

    update msg

--- msg.py
+++ msg.py
@@ -1,18 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 def msg(alert_json):
-    have_instance = False
     message = f"*[{alert_json['status'].upper()}] {alert_json['labels']['alertname']}*\n"
-    if (('summary' in alert_json['annotations'] and alert_json['labels']['instance'] in alert_json['annotations']['summary'])
-        or ('description' in alert_json['annotations'] and alert_json['labels']['instance'] in alert_json['annotations']['description'])):
-        have_instance = True
     for key in alert_json['labels']:
-        if key == 'alertname':
-            continue
-        elif key == 'instance' and have_instance:
-            continue
-        else:
-            message = message + f" {key}: `{alert_json['labels'][key]}`\n"
+        message = message + f" {key}: `{alert_json['labels'][key]}`\n"
     if 'summary' in alert_json['annotations'] and alert_json['annotations']:
         message = message + f" *summary*: `{alert_json['annotations']['summary']}`\n"
     if 'description' in alert_json['annotations'] and alert_json['annotations']:
