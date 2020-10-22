a288f3a32181bcbde6457abb23339f6c7e757c60
tinhgin <tinhhn.uit@gmail.com>
2020-10-22 16:50:14 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,5 +2,6 @@
 
 sed -i s/botToken/"$botToken"/ alert.py
 sed -i s/chatID/"$chatID"/ alert.py
+[ "$header" == "rule" ] && sed -i s/alertname/"$header"/ msg.py
 
 gunicorn -w 4 -b 0.0.0.0:9165 alert:app
